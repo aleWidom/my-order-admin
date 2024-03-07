02895a81d5092578a8faf48d98118a53eb1b573d
alejandro <widomlanski@hotmail.com>
2024-03-07 09:20:38 -0300

    table actives

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ const HomePage = () => {
   return (
     <div className={styles.container}>
       <AdminOrdersActives />
-      {/*     <AdminTablesActives /> */}
+      <AdminTablesActives />
     </div>
   );
 };
